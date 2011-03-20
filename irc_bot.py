c81f5cc1ec19a59d263a7f76198c1161e9525872
Fabio Santos <supergrilo@gmail.com>
2011-03-20 03:39:33 -0300

    ainda não consegui, mas vou continuar tentando :(

--- irc_bot.py
+++ irc_bot.py
@@ -5,6 +5,12 @@ import ConfigParser
 import irclib
 
 
+def handleJoin( connection, event ):
+    nick = event.source().split('!')[0]
+    but3k4_msg = "Papai mandou lembrancas"
+    if nick.find('fabio') != -1 or nick.find('miguim') != -1:
+        ''.privmsg( conf['channel'], but3k4_msg )
+
 class IRC_Bot:
     def __init__(self):
         self.config = ConfigParser.RawConfigParser()
@@ -24,6 +30,7 @@ class IRC_Bot:
             conf[key] = self.config.get( 'irc_bot', key )
  
         try:
+#            irclib.DEBUG = conf['debug']
             irc = irclib.IRC()
             server = irc.server()
             server.connect( conf['network'], int(conf['port']), conf['nick'], username = conf['username'], ircname = conf['ircname'] )
@@ -31,7 +38,7 @@ class IRC_Bot:
         except Exception, ex:
             print "Xi cagou: %s" % ex
             sys.exit(1)
-        
+        irc.add_global_handler( 'join', handleJoin ) 
         irc.process_forever()
 
 irc_bot = IRC_Bot()
