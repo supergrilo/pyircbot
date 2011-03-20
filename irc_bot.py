61ef16bc0ed769d3edbdb2a060d47c3aa977a9c2
Fabio Santos <supergrilo@gmail.com>
2011-03-20 02:41:24 -0300

    Aprender python sem doc? com classe! hehe :D

--- irc_bot.py
+++ irc_bot.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python
 
+import sys
 import ConfigParser
 import irclib
 
-config = ConfigParser.RawConfigParser()
-config.read('conf/irc_bot.conf')
+class IRC_Bot:
+    def __init__(self):
+        self.config = ConfigParser.RawConfigParser()
+        self.config.read( 'conf/irc_bot.conf' )
 
-conf = {
-        'network': None,
-        'port': None,
-        'nick': None,
-        'username': None,
-        'ircname': None,
-        'debug': None
-}
+        conf = {
+                    'network': None,
+                    'port': None,
+                    'nick': None,
+                    'username': None,
+                    'ircname': None,
+                    'debug': None
+        }
 
-for key in conf.keys():
-    conf[key] = config.get('irc_bot', key)
+        for key in conf.keys():
+            conf[key] = self.config.get( 'irc_bot', key )
 
-try:
-    irc = irclib.IRC()
-    server = irc.server()
-    server.connect( conf['network'], int(conf['port']), conf['nick'], username = conf['username'], ircname = conf['ircname'] )
-except Exception, ex:
-    print "Xi cagou: %s" % ex
+        try:
+            irc = irclib.IRC()
+            server = irc.server()
+            server.connect( conf['network'], int(conf['port']), conf['nick'], username = conf['username'], ircname = conf['ircname'] )
+        except Exception, ex:
+            print "Xi cagou: %s" % ex
+            sys.exit(1)
         
-irc.process_forever()
+        irc.process_forever()
+
+irc_bot = IRC_Bot()
