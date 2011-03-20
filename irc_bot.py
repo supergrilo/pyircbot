a6331bcc722fac242c26b15fed4d29c7e3f79967
Fabio Santos <supergrilo@gmail.com>
2011-03-20 01:14:47 -0300

    Agora escrito com irclib, e batizado de dinobot

--- irc_bot.py
+++ irc_bot.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python
 
-import socket
+import irclib
 
 network="irc.oftc.net"
 port=6667
-nick="pyircbot"
+nick="dinobot"
 ident="pybot"
 realname="Bot do Fabio"
 channel="#sysadms"
 
-irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-irc.connect(( network, port ))
-print irc.recv(4096)
-irc.send('nick '+nick+'\r\n')
-irc.send('USER %s %s %s :Python IRC\r\n' % (ident, realname, ident))
-irc.send('JOIN '+channel+'\r\n')
+irclib.DEBUG = True
 
-while 1:
-    line=irc.recv(1024)
-    print line
-    if line.find('Welcome to the OFTC Internet Relay Chat Network') != -1:
-        irc.send('JOIN '+channel+'\r\n')
-    if line.find('PING') != -1:
-        irc.send('PONG '+line[1]+'\r\n')
+irc = irclib.IRC()
+
+server = irc.server()
+server.connect( network, port, nick, ircname = realname )
+server.join( channel )
+
+server.privmsg( channel, 'Querida CHEGUEI!' )
+
+
+irc.process_forever()
